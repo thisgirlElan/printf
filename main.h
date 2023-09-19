cd6a27689f29f7465607bda7c7352dc7b8568cf6
Elan <elankatheryne@gmail.com>
2023-09-19 04:21:40 +0300

    add main header file

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+
+int _putchar(char c);
+void print_binary(unsigned int num);
+char rot13(char c);
+int _printf(const char *format, ...);
+
+#endif /* MAIN_H */
