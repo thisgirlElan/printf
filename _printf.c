042364258d07214a268bb4260f6a6f7938cf5ce0
Elan <elankatheryne@gmail.com>
2023-09-21 01:40:09 +0300

    minor fix :b

--- _printf.c
+++ _printf.c
@@ -23,8 +23,8 @@ return (write(1, &c, 1));
 void print_binary(unsigned int num)
 {
 if (num > 1)
-print_binary(num >> 1);
-putchar('0' + num & 2);
+print_binary(num / 2);
+_putchar('0' + num % 2);
 }
 
 /**
