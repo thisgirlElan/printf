2f5dbc539650acc48c8e72cfc3a84a1fa6e22ecf
Elan <elankatheryne@gmail.com>
2023-09-21 03:13:48 +0300

    minor fix

--- _printf.c
+++ _printf.c
@@ -93,52 +93,15 @@ buffer_index = 0;
 else
 {
 format++;
-int has_plus = 0;
-int has_space = 0;
-int has_hash = 0;
-
-while (*format == '+' || *format == ' ' || *format == '#')
-{
-if (*format == '+')
-has_plus = 1;
-else if (*format == ' ')
-has_space = 1;
-else if (*format == '#')
-has_hash = 1;
-
-format++;
-}
 
 switch (*format)
 {
 case 'c':
-if (has_plus)
-{
-char ch = va_arg(args, int);
-putchar(ch);
-printed_chars++;
-}
-else
-{
 putchar(va_arg(args, int));
 printed_chars++;
-}
 break;
 case 's':
-if (has_plus)
-{
-char *str = va_arg(args, char *);
-while (*str != '\0')
-{
-putchar(*str);
-printed_chars++;
-str++;
-}
-}
-else
-{
 printed_chars += printf("%s", va_arg(args, char *));
-}
 break;
 case 'd':
 case 'i':
@@ -164,6 +127,7 @@ snprintf(buffer + buffer_index, sizeof(buffer) - buffer_index, "0x%lx", (unsigne
 buffer_index += strlen(buffer + buffer_index);
 printed_chars += 2;
 }
+break;
 break;
 case 'X':
 printed_chars += printf("%X", va_arg(args, unsigned int));
